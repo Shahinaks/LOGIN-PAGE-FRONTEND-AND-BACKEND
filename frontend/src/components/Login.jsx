c8de4af480bf544ec44a89c2f9d1a223f5ff2448
Shahina <shahina.sharafudeenki@gmail.com>
2025-04-23 23:39:21 +0530

    Updated to use REACT_APP_API_URL

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-        const response = await axios.post('http://localhost:5000/login', {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
         email,
         password,
       });
